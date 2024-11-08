c34a31f0199996f414378bcd6cdf7a5d0be584df
Andiscolemon <andiscolemon@gmail.com>
2024-11-08 11:12:45 +0200

    Reverted changes to background track. Problem is on GST backend.

--- mopidy_zestbox/frontend.py
+++ mopidy_zestbox/frontend.py
@@ -49,25 +49,15 @@ class ZestboxFrontend(pykka.ThreadingActor, CoreListener):
         self.zestbox.playing_user_track = False
         if not self.zestbox.background_playlist:
             return
-        uris_to_add = []
-        for entry in self.zestbox.background_playlist:
-            uri = entry
-            if "playlist" in entry:
-                tracks = self.core.library.lookup([entry]).get()
-                for track in tracks:
-                    uris_to_add.append(track)
-            else:
-                uris_to_add.append(uri) 
 
-        self.core.tracklist.add(uris=uris_to_add)
-        self.core.tracklist.add(uris=uris_to_add)
+        self.core.tracklist.add(uris=self.zestbox.background_playlist)
         self.core.tracklist.set_consume(False)
         self.core.tracklist.set_random(True)
         self.core.tracklist.set_repeat(True)
         if self.core.playback.get_state().get() == "stopped":
             self.core.playback.play()
-        self.logger.info(f"Added background tracks.\nAdded tracks: {uris_to_add}")
-        self.logger.info(f"Added background tracks.\nAdded tracks: {uris_to_add}")
+        self.logger.info(f"Added background tracks.\nAdded tracks: {self.core.library.lookup(self.zestbox.background_playlist).get()}")
+
 
     def add(self, new_uris = [], requester = ""):
         try:
